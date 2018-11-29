6a4e92802523929a0b7b09ea041b874ec0e36b11
casetta-smeup <francesco.casetta@smeup.com>
2018-11-29 11:42:54 +0100

    sorting

--- src/components/smeup-mat/smeup-mat.tsx
+++ src/components/smeup-mat/smeup-mat.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop } from '@stencil/core';
+import { Component, Prop, State } from '@stencil/core';
 
 @Component({
   tag: 'smeup-mat',
@@ -7,22 +7,61 @@ import { Component, Prop } from '@stencil/core';
 })
 export class SmeupMatrix {
   @Prop() columns: any[];
-  
+
   @Prop() rows: any[];
+
+  @State() sort = {
+    code: "",
+    sortMode: ""
+  };
+
+  onSort(c) {
+    // switch sort
+    c.sortMode = c.sortMode === "A" ? "D" : "A";
+
+    this.sort = {
+      code: c.code,
+      sortMode: c.sortMode
+    }
+  }
+
+  sortRows(_rows: any[]) {
+    const filteredRows = _rows.filter((r: any) => !r.group);
+
+    if (filteredRows.length == 0) {
+      return _rows;
+    }
+
+    return _rows.sort((r1, r2) => {
+      const val1 = r1.fields[this.sort.code].smeupObject.codice;
+      const val2 = r2.fields[this.sort.code].smeupObject.codice;
+
+      // check if ascending or descending sort
+      const sortMode = this.columns.filter(c => c.code === this.sort.code)[0]
+        .sortMode;
+
+      const compare = val1.localeCompare(val2);
+      return sortMode === "A" ? compare : compare * -1;
+    });
+  }
 
   render() {
     // header
     let thead = null;
     if (this.columns) {
       thead = this.columns.map(c => (
-        <th>{c.code}</th>
+        <th onClick={this.onSort.bind(this, c)}>{c.code}</th>
       ))
     }
 
-    // TODO rows
     let tbody = null;
     if (this.rows) {
-      tbody = this.rows.map(r => (
+      let sortedRows = this.rows;
+      if (this.sort.code) {
+        sortedRows = this.sortRows(this.rows);
+      }
+
+      tbody = sortedRows.map(r => (
         <tr>{this.columns.map(c => (
           <td>{r.fields[c.code].smeupObject.codice}</td>
         ))}</tr>
