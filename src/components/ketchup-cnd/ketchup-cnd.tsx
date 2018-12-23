3d1fd996a03cb365f32c56189236ac391cc263c1
casetta-smeup <francesco.casetta@smeup.com>
2018-12-23 11:58:10 +0100

    added background circle

--- src/components/ketchup-cnd/ketchup-cnd.tsx
+++ src/components/ketchup-cnd/ketchup-cnd.tsx
@@ -9,20 +9,55 @@ export class KetchupCnd {
   private ctx: CanvasRenderingContext2D
   private updateBind = this.update.bind(this)
   private start: number
+  private requestAnimationFrame: number
 
   componentDidLoad() {
     // starting time
     this.start = new Date().getTime()
 
     this.ctx = this.canvas.getContext('2d')
-    requestAnimationFrame(this.updateBind)
+
+    // rotating the canvas
+    // this.ctx.rotate((45 * Math.PI) / 180)
+
+    this.update()
   }
 
   update() {
-    requestAnimationFrame(this.updateBind)
+    this.requestAnimationFrame = requestAnimationFrame(this.updateBind)
 
     this.ctx.clearRect(0, 0, 100, 100)
 
+    // draw circles
+    this.drawBackgroundCircle()
+    this.drawForegroundCircle()
+  }
+
+  drawBackgroundCircle() {
+    this.ctx.beginPath()
+    this.ctx.lineWidth = 5
+    this.ctx.strokeStyle = '#dddddd'
+    this.ctx.arc(50, 50, 45, 0, Math.PI * 2)
+    this.ctx.stroke()
+  }
+
+  drawForegroundCircle() {
+    const endAngle = this.getEndAngle()
+
+    if (endAngle > 0) {
+      // draw cirlce
+      this.ctx.beginPath()
+      this.ctx.lineWidth = 5
+      this.ctx.strokeStyle = '#369491'
+      this.ctx.arc(50, 50, 45, 0, endAngle)
+      this.ctx.stroke()
+    } else {
+      console.log('stopping animate')
+      cancelAnimationFrame(this.requestAnimationFrame)
+    }
+  }
+
+  getEndAngle() {
     const max = 5000 // 5 secondi
 
     const now = new Date().getTime()
@@ -34,11 +69,7 @@ export class KetchupCnd {
 
     const toBeRemoved = (fullCircle * percentage) / 100
 
-    // draw circle
-    this.ctx.lineWidth = 10
-    this.ctx.beginPath()
-    this.ctx.arc(50, 50, 45, 0, fullCircle - toBeRemoved)
-    this.ctx.stroke()
+    return fullCircle - toBeRemoved
   }
 
   render() {
